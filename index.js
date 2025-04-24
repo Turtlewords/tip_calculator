0fefc8ded71bcd6054271fc7ba76a1b201721c5a
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-04-24 13:07:34 -0500

    begin calculator functions

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const bill = document.querySelector("#bill");
 const people = document.querySelector("#people");
-const tip = document.querySelector("#tip");
+const tipEl = document.querySelector("#tip");
 const total = document.querySelector("#total");
 const resetBtn = document.querySelector("#reset-btn");
 
 const buttons = document.querySelectorAll("button"); 
+
+console.log(buttons);
 
 buttons.forEach((button) => {
     button.addEventListener("click", (e) => {
@@ -12,10 +14,34 @@ buttons.forEach((button) => {
     })
 })
 
+for (let i = 0; i < buttons.length - 2; i++) {
+    buttons[i].addEventListener("click", calculateTip);
+}
+
+people.addEventListener("change", countPeople);
+
 resetBtn.addEventListener("click", () => {
     bill.value = "";
     people.value = "";
-    tip.textContent = "$0.00";
+    tipEl.textContent = "$0.00";
     total.textContent = "$0.00";
 })
+
+function countPeople() {
+    let num = Number(people.value)
+
+    total.textContent = "$" + String((bill.value / num).toFixed(2));
+}
+
+function calculateTip() {
+    let tip = bill.value * this.dataset.percent;
+    let num = Number(people.value)
+    console.log("Tip: " + tip);
+    let total = Number(bill.value) + tip;
+    console.log("Total: " + total)
+
+    tipEl.textContent = "$" + tip;
+    total.textContent = "$" + (total / num).toFixed(2);
+}
+
 
