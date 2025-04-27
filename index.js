cd3f1c8656cfefe4a7263c72b93b09a10021190d
turtlewords <144561651+Turtlewords@users.noreply.github.com>
2025-04-27 10:38:14 -0500

    fix reset button

--- index.js
+++ index.js
@@ -34,6 +34,13 @@ resetBtn.addEventListener("click", () => {
     people.value = "";
     tipEl.textContent = "$0.00";
     totalEl.textContent = "$0.00";
+    customBtn.textContent = "Custom";
+    peopleError.style.display = "none"
+    people.style.outline = "none";
+    buttons.forEach((button) => {
+        button.classList.remove("tip-active");
+    })
+
 })
 
 function calculateTotal() {
